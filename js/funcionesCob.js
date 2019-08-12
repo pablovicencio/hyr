1634736d87dcb5371f9a2bb730d64ec5bd3e0b09
pablovicencio <pablo.vicencio@correoaiep.cl>
2019-08-12 13:01:55 -0400

    Update funcionesCob.js

--- js/funcionesCob.js
+++ js/funcionesCob.js
@@ -136,7 +136,7 @@ $(document).ready(function() {
           numeral(result[i].pagos).format('$000,000,000,000') + "</td><td>" +
           numeral(result[i].saldo).format('$000,000,000,000') + "</td><td>" +
           numeral(result[i].prom_dias_pago).format('000,000,000,000') + "</td><td>" +
-          '<a id="btn_modal_det_inf_cob" class="link-modal btn btn-outline-success" data-id="'+result[i].id_emp+'" data-tipo="1" data-rut="'+result[i].rut_emp+'" data-emp="'+result[i].razon_social_emp+'" data-prom="'+result[i].prom_dias_pago+'"  data-toggle="modal" ><i class="fa fa-plus-square" aria-hidden="true"></i></a></td></tr>'
+          '<a id="btn_modal_det_inf_cob" class="link-modal btn btn-outline-success" data-id="'+result[i].id_emp+'" data-tipo="1" data-rut="'+result[i].rut_emp+'" data-emp="'+result[i].razon_social_emp+'" data-prom="'+result[i].prom_dias_pago+'"  data-toggle="modal" ><i class="fa fa-plus-square" aria-hidden="true"></i></a></td><td>'
           '<a id="btn_modal_det_inf_cob" class="link-modal btn btn-outline-warning" data-id="'+result[i].id_emp+'" data-tipo="2" data-rut="'+result[i].rut_emp+'" data-emp="'+result[i].razon_social_emp+'" data-prom="'+result[i].prom_dias_pago+'"  data-toggle="modal" ><i class="fa fa-plus-square" aria-hidden="true"></i></a></td></tr>'
           $("#tabla_inf_cob").append(nuevafila);
 
